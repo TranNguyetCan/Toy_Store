4bdfb6817a42511589fe1e329e3ddd9a0868d7a4
TranNguyetCan <Cantngcc210146@fpt.edu.vn>
2023-06-17 15:58:35 +0700

    update login

--- Login.php
+++ Login.php
@@ -27,34 +27,34 @@
             </style>
         </button>
         <?php
-        // include_once("connect.php");
-        // session_start();
+        include_once("connect.php");
+        session_start();
 
-        // if (isset($_POST['btnLogin'])) {
-        //     if (isset($_POST['pass']) && isset($_POST['email'])) {
-        //         echo $pass = $_POST['pass'];
-        //         echo $email = $_POST['email'];
-        //         $c = new Connect();
-        //         $dblink = $c->connectToPDO();
-        //         $sql = "SELECT * FROM users WHERE email = ? and password = ?";
-        //         $stmt = $dblink->prepare($sql);
-        //         $re = $stmt->execute(array("$email", "$pass"));
-        //         $numrow = $stmt->rowCount();
-        //         $row = $stmt->fetch(PDO::FETCH_BOTH);
-        //         if ($numrow == 1) {
-        //             echo "Login successfully";
-        //             setcookie("cc_username", $row['fullname'], time() + 3600);
-        //             setcookie("cc_id", $row['id'], time() + 3600);
-        //             $_SESSION['id'] =  $row['id'];
-        //             echo "Hello world";
-        //             // header("Location: HomePage1.php");
-        //         } else {
-        //             echo "Something wrong with your info<br>";
-        //         }
-        //     } else {
-        //         echo "Please enter your info";
-        //     }
-        // }
+        if (isset($_POST['btnLogin'])) {
+            if (isset($_POST['pass']) && isset($_POST['email'])) {
+                echo $pass = $_POST['pass'];
+                echo $email = $_POST['email'];
+                $c = new Connect();
+                $dblink = $c->connectToPDO();
+                $sql = "SELECT * FROM users WHERE email = ? and password = ?";
+                $stmt = $dblink->prepare($sql);
+                $re = $stmt->execute(array("$email", "$pass"));
+                $numrow = $stmt->rowCount();
+                $row = $stmt->fetch(PDO::FETCH_BOTH);
+                if ($numrow == 1) {
+                    echo "Login successfully";
+                    setcookie("cc_username", $row['fullname'], time() + 3600);
+                    setcookie("cc_id", $row['id'], time() + 3600);
+                    $_SESSION['id'] =  $row['id'];
+                    echo "Hello world";
+                    // header("Location: HomePage1.php");
+                } else {
+                    echo "Something wrong with your info<br>";
+                }
+            } else {
+                echo "Please enter your info";
+            }
+        }
         /*
 Put your code right here
 1. Check if button 'btnLogin' is submitted, if it's true, go to 2.
@@ -70,9 +70,9 @@ Put your code right here
         ?>
         <div class="container">
             <h2 class="pt-3">Member Login</h2>
-            <form id="form1" name="form1" method="POST" action="actionLogin.php">
+            <form id="form1" name="form1" method="POST" action="login.php">
                 <p style="color:Orange"><?php echo $result ?? "" ?></p>
-                <form action=" actionLogin.php " method="POST">
+                <form action=" index.php " method="POST">
                     <div class="row">
                         <div class="form-group">
                             <label for="txtEmail" class="col-sm-2 control-label">Email(*): </label>
